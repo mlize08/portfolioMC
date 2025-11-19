b39d17b23498a651735bb1a44c100ceb40f704f7
Monalize M <150396638+mlize08@users.noreply.github.com>
2025-11-19 10:09:08 -0300

    condicção

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ let texto =  document.querySelector("#texto");
 texto.textContent = "Monalize";
 
 let nome = prompt("Qual o seu nome?");
+if (nome == null){
+    texto.textContent = "Seja bem-vindo(a)!";
+}
+else{
 texto.textContent = nome;
+}
